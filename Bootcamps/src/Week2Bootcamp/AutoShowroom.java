7e9182d97aaef3d4245ef9d97f77152a1dc85d7a
yoo <ykog0002@student.monash.edu>
2021-08-05 14:06:54 +0800

    Week 2 Lab complete, update version with constructor

--- Bootcamps/src/Week2Bootcamp/AutoShowroom.java
+++ Bootcamps/src/Week2Bootcamp/AutoShowroom.java
@@ -3,17 +3,10 @@ package Week2Bootcamp;
 public class AutoShowroom {
     Car[] carArray = new Car[3];
     public void createCars(){
-        carArray[0] = new Car();
-        carArray[0].setMake("BMW");
-        carArray[0].setModel("X7");
+        carArray[0] = new Car("BMW", "XY");
+        carArray[1] = new Car("Audi", "A8");
+        carArray[2] = new Car("Mercedes", "GLS");
 
-        carArray[1] = new Car();
-        carArray[1].setMake("Audi");
-        carArray[1].setModel("A8");
-
-        carArray[2] = new Car();
-        carArray[2].setMake("Mercedes");
-        carArray[2].setModel("GLS");
     }
     public void printStatus(){
         System.out.println("Welcome to FIT2099 Showroom");
--- Bootcamps/src/Week2Bootcamp/Car.java
+++ Bootcamps/src/Week2Bootcamp/Car.java
@@ -1,17 +1,15 @@
 package Week2Bootcamp;
 
 public class Car {
-    private String make;
-    private String model;
+    String make;
+    String model;
+
+    public Car(String make, String model){
+        this.make = make;
+        this.model = model;
+    }
     public String getCarDescription(){
         return "Maker:" + make + " and" + " Model:" + model;
     }
 
-    public void setMake(String make) {
-        this.make = make;
-    }
-
-    public void setModel(String model) {
-        this.model = model;
-    }
 }

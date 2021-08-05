da8e3e07e8e55aec0727ce6b8c4315a8bdff1536
yoo <ykog0002@student.monash.edu>
2021-08-05 13:04:53 +0800

    temporary

---
+++ Bootcamps/src/Week2Bootcamp/AutoShowroom.java
@@ -0,0 +1,8 @@
+package Week2Bootcamp;
+
+public class AutoShowroom {
+    public void printStatus(){
+        System.out.println("Welcome to FIT2099 Showroom");
+        System.out.println("Thank you for visiting FIT2099 Showroom");
+    }
+}
---
+++ Bootcamps/src/Week2Bootcamp/AutoShowroomDriver.java
@@ -0,0 +1,8 @@
+package Week2Bootcamp;
+
+public class AutoShowroomDriver {
+    public static void main(String[] args) {
+        AutoShowroom showroom = new AutoShowroom();
+        showroom.printStatus();
+    }
+}
